76187316e88ca5dc07f567f1d8039b16b6159516
ArturoAlmagroMendez <almagromendezarturo@gmail.com>
2023-04-21 13:04:49 +0200

    make more reusable tabs.js

--- js/admin/admin.js
+++ js/admin/admin.js
@@ -1,16 +1,34 @@
 export default(() =>{
-
-    let tabcontent = document.getElementById('images');
-    tabcontent.style.display = "none";
-
-    let tablinks = document.querySelectorAll(".admin-content-login-tab");
-
-    tablinks.forEach(tablink => {
-        tablink.addEventListener('click', () => {
-          console.log("tabtab");
-          let dataSet = tablink.dataset.set;
-          document.getElementById(dataSet).style.display = "block";
+    const editButtons = document.querySelectorAll('.edit-button');
+    const deleteButtons = document.querySelectorAll('.delete-button');
+    const hamburger = document.querySelector('.admin-header-hamburger');
+    const dropdownButton = document.querySelector('.admin-bar-icon');
+    const cleanButton = document.querySelector('.admin-content-login-tabs-button-clean');
+    const saveButton = document.querySelector('.admin-content-login-tabs-button-save');
+    
+      editButtons.forEach(editButton =>{
+        editButton.addEventListener('click' , () => {
+          console.log("Se ha presionado el botón para editar");
         });
-      });  
+      });
+      deleteButtons.forEach(deleteButton =>{
+        deleteButton.addEventListener('click' , () => {
+          console.log("Se ha presionado el botón para borrar");
+        });
+      });
+      hamburger.addEventListener('click' , () =>{
+        console.log("Has clickado el menu hamburguesa");
+      })
+      dropdownButton.addEventListener('click' , () =>{
+        console.log("Has clickado el boton desplegable");
+      })
+      cleanButton.addEventListener('click' , () =>{
+        console.log("Has clickado el boton de limpiar");
+      })
+      saveButton.addEventListener('click' , () =>{
+        console.log("Has clickado el boton de guardar");
+      })
+        
+
 })();
   
